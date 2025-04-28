ea66cc235b1eb26c8f384c66f2c8b75bc4864a4f
Yuxuan-Zhang-Dexter <116124872+Yuxuan-Zhang-Dexter@users.noreply.github.com>
2025-04-28 10:20:07 -0700

    sanity check

--- games/ace_attorney/ace_agent.py
+++ games/ace_attorney/ace_agent.py
@@ -103,16 +103,16 @@ def main():
     thinking_bool = str2bool(args.thinking)
 
     print("--------------------------------Start Evidence Worker--------------------------------")
-    # evidence_result = ace_evidence_worker(
-    #     system_prompt,
-    #     args.api_provider,
-    #     args.model_name,
-    #     prev_response,
-    #     thinking=thinking_bool,
-    #     modality=args.modality,
-    #     episode_name=args.episode_name,
-    #     cache_dir=cache_dir
-    # )
+    evidence_result = ace_evidence_worker(
+        system_prompt,
+        args.api_provider,
+        args.model_name,
+        prev_response,
+        thinking=thinking_bool,
+        modality=args.modality,
+        episode_name=args.episode_name,
+        cache_dir=cache_dir
+    )
     decision_state = None
 
     try:
